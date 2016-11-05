ea7b48a0b1ed072186ed199722366dc6c5da8919
Sauviage <erhardclaire@gmail.com>
2016-11-05 18:26:33 +0100

    Premier jet : menu mais pb avec les images dropbox

--- index.php
+++ index.php
@@ -2,18 +2,33 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Teatime tout sur le thé</title>
+    <title>Laure De Malibran</title>
     <meta name="description" content="Tous les thés du monde: thés verts, noirs, blancs et autres/// Produits dérivés: théières, tasses"/>
     <meta name="keywords" content="thé, thé vert, thé noir, thé blanc, théières, tasses"/>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link href="style.css" rel=stylesheet media="screen"/>
+    <link href="css/style.css" rel=stylesheet media="screen"/>
 </head>
 <body>
+    <div id="menu">
+        <img class="logo" src="https://www.dropbox.com/s/rdi3f53np3nkkxw/logoldm.png?dl=0" alt="Logo Laure De Malibran"/>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Biographie</a></li>
+            <li><a href="#">Galerie</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <a class="reseaux" href="#"><img src="https://photos-5.dropbox.com/t/2/AADShBeNTWE36GpOSKFXXMbVddwxukIzZ8EKt-ubEz95Ww/12/286381322/png/32x32/1/_/1/2/Facebook.png/EIS5lfIEGOZwIAIoAg/rdzWTL9CSk_K0drNCfKylqoncPs1d17vWILkk6gU8Oc?size=2048x1536&size_mode=3" alt="Reseaux sociaux : Facebook"/></a>
+        <a class="reseaux" href="#"><img src="https://www.dropbox.com/s/6lzfe8jt36kctst/Pinterest.png?dl=0" alt="Reseaux sociaux : Pinterest"/></a>
+        <p>Copyright &copy;<br>
+            <a href="#">Pierre-Yves Lotode</a> - <a href="#">Kevin Alary</a> - <a href="#">Claire Erhard</a></p>
+
+    </div>
+
 
 </body>
 </html>
