c2d9a6b065591d7374c8ffc9a9f6a9c114d16230
Sauviage <erhardclaire@gmail.com>
2016-11-05 16:07:59 +0100

    php c mieux lol

--- index.php
+++ index.php
@@ -2,13 +2,16 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Teatime tout sur le thé</title>
+    <meta name="description" content="Tous les thés du monde: thés verts, noirs, blancs et autres/// Produits dérivés: théières, tasses"/>
+    <meta name="keywords" content="thé, thé vert, thé noir, thé blanc, théières, tasses"/>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <link href="style.css" rel=stylesheet media="screen"/>
 </head>
 <body>
 
