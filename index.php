b2b935793b6b6df7afc540d847f577a2e8b7d33c
Sauviage <erhardclaire@gmail.com>
2016-11-17 16:55:02 +0100

    commit
    Menu :
    - RESEAUX OK ! (manque les marge dessus/dessous
    
    Article :
    - Toujours le pb de l'image
    
    Derniers notés :
    - Pb d'affichage du dessous

--- index.php
+++ index.php
@@ -14,33 +14,60 @@
     <link href="css/style.css" rel=stylesheet media="screen"/>
 </head>
 <body>
-    <header>
-        <div id="menu">
-            <img class="logo" src="images/logoldm.png" alt="Logo Laure De Malibran"/>
-            <ul>
-                <li class="choix"><a href="#">Accueil</a></li>
-                <li><a href="#">Biographie</a></li>
-                <li><a href="#">Galerie</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-            <div id="reseaux">
-                <a class="reseaux" href="#"><img src="images/facebook.png" alt="Reseaux sociaux : Facebook"/></a>
-                <a class="reseaux" href="#"><img src="images/pinterest.png" alt="Reseaux sociaux : Pinterest"/></a>
-            </div>
-            <p>Copyright &copy;<br>
-                <a href="#">Pierre-Yves Lotode</a> - <a href="#">Kevin Alary</a> - <a href="#">Claire Erhard</a></p>
+<header>
+    <div id="menu">
+        <img class="logo" src="images/logoldm.png" alt="Logo Laure De Malibran"/>
+        <ul>
+            <a href="#"><li class="choix triangle">Accueil</li></a>
+            <li><a href="#">Biographie</a></li>
+            <li><a href="#">Galerie</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <div id="reseaux">
+            <a class="reseaux" href="#"><img src="images/facebook.png" alt="Reseaux sociaux : Facebook"/></a>
+            <a class="reseaux" href="#"><img src="images/pinterest.png" alt="Reseaux sociaux : Pinterest"/></a>
         </div>
-    </header>
-    <section>
-        <h1>A la une</h1>
-        <article id="article">
-            <img src="images/africaine.jpg">
+        <p>Copyright &copy;<br>
+            <a href="#">Pierre-Yves Lotode</a> - <a href="#">Kevin Alary</a> - <a href="#">Claire Erhard</a></p>
+    </div>
+</header>
+<section>
+    <h1>A la une</h1>
+    <article id="article">
+        <img src="images/africaine.jpg">
+        <div id="contenu-article">
             <h3>Titre article</h3>
             <h4>> 30 octobre 2016</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel ligula quis velit mattis cursus a eu ex. Etiam tempus tristique ligula quis laoreet. Aliquam vitae rutrum mi. Nulla tincidunt id justo vitae accumsan. Mauris tempor ac lorem eget sodales. Quisque gravida, leo eu feugiat ullamcorper, leo sem tempus orci, non aliquam nibh lacus eu turpis. Donec ut feugiat felis. Cras vel neque eu erattest malesuada venenatis vitae vitae nulla. Quisque mollis in risus eu varius.<br><br><span><a href="#">En savoir plus</a></span> </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel ligula quis velit mattis cursus a eu ex. Etiam tempus tristique ligula quis laoreet. Aliquam vitae rutrum mi. Nulla tincidunt id justo vitae accumsan. Mauris tempor ac lorem eget sodales. Quisque gravida, leo eu feugiat ullamcorper, leo sem tempus orci, non aliquam nibh lacus eu turpis. Donec ut feugiat felis. Cras vel neque eu erat malesuada venenatis vitae vitae nulla. Quisque mollis in risus eu varius.<br><br><span><a href="#">En savoir plus</a></span></p>
 
-        </article>
-    </section>
+
+        </div>
+
+    </article>
+</section>
+<aside>
+    <h2>Derniers notés</h2>
+    <article id="dernier-note">
+
+        <img src="images/africaine.jpg">
+        <div id="dessous">
+            <ul>
+                <li class="titre-tableau">Africaine</li>
+                <li class="commentaire">3</li>
+                <li class="coeur">3</li>
+                <li class="aimer">3</li>
+
+            </ul>
+            <!--<h4>Africaine</h4>-->
+            <!--<div class="commentaire">3</div>-->
+            <!--<div class="coeur">2</div>-->
+            <!--<div class="aimer">6</div>-->
+            <!--<img src="">-->
+            <!--<img src="">-->
+            <!--<img src="">-->
+        </div>
+    </article>
+</aside>
 
 </body>
 </html>
