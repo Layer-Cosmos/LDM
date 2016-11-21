b9bdb7852148e77f2263329bd207cbd42689e418
Sauviage <erhardclaire@gmail.com>
2016-11-21 19:04:52 +0100

    Menu : - Le rend en hauteur 100% - Fuckin flèche de mise !
    Section & Aside :
    - a mettre : overflow : scroll

--- index.php
+++ index.php
@@ -1,36 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Laure De Malibran</title>
-    <meta name="description" content="Tous les thés du monde: thés verts, noirs, blancs et autres/// Produits dérivés: théières, tasses"/>
-    <meta name="keywords" content="thé, thé vert, thé noir, thé blanc, théières, tasses"/>
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <link href="css/style.css" rel=stylesheet media="screen"/>
-</head>
-<body>
-<header>
-    <div id="menu">
-        <img class="logo" src="images/logoldm.png" alt="Logo Laure De Malibran"/>
-        <ul>
-            <a href="#"><li class="choix triangle">Accueil</li></a>
-            <li><a href="#">Biographie</a></li>
-            <li><a href="#">Galerie</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-        <div id="reseaux">
-            <a class="reseaux" href="#"><img src="images/facebook.png" alt="Reseaux sociaux : Facebook"/></a>
-            <a class="reseaux" href="#"><img src="images/pinterest.png" alt="Reseaux sociaux : Pinterest"/></a>
-        </div>
-        <p>Copyright &copy;<br>
-            <a href="#">Pierre-Yves Lotode</a> - <a href="#">Kevin Alary</a> - <a href="#">Claire Erhard</a></p>
-    </div>
-</header>
+<?php include 'menu.php'; ?>
 <section>
     <h1>A la une</h1>
     <article id="article">
@@ -44,6 +12,7 @@
         </div>
 
     </article>
+
 </section>
 <aside>
     <h2>Derniers notés</h2>
