b12ce109827a54629514c1881b21a14360b5c880
Sauviage <erhardclaire@gmail.com>
2016-11-27 17:52:30 +0100

    nbiop

--- biographie.php
+++ biographie.php
@@ -1,7 +1,7 @@
 <?php include 'menu.php'; ?>
-<section id="bio">
+<section id="bio" class="col-lg-offset-1 col-lg-9">
     <article>
-        <img id="portrait" src="" alt=""/>
+        <img id="portrait" src="http://www.dannyst.com/blogimg/gallery-portraits-of-strangers-20.jpg" alt=""/>
         <h1>Biographie</h1>
         <p><span>"Novo denique perniciosoque"</span>Exemplo idem Gallus ausus est inire flagitium grave, quod Romae cum ultimo dedecore temptasse aliquando dicitur Gallienus, et adhibitis paucis clam ferro succinctis vesperi per tabernas palabatur et conpita quaeritando Graeco sermone, cuius erat inpendio gnarus, quid de Caesare quisque sentiret. et haec confidenter agebat in urbe ubi pernoctantium luminum claritudo dierum solet imitari fulgorem. postremo agnitus saepe iamque, si prodisset, conspicuum se fore contemplans, non nisi luce palam egrediens ad agenda quae putabat seria cernebatur. et haec quidem medullitus multis gementibus agebantur.
 
