8d438dfc83a44790dd6eed65487a188f8da3e6b4
Sauviage <erhardclaire@gmail.com>
2016-12-13 15:33:36 +0100

    Add files via upload
    Changement du noms des class et id sur :
    - menu.php
    - index.php
    Correction rendu visuel avec bootstrap

--- index.php
+++ index.php
@@ -1,70 +1,85 @@
 <?php include 'menu.php'; ?>
-<section  class="col-lg-offset-1 col-lg-5">
-    <h1>A la une</h1>
-    <article id="article">
-        <div id="image"><img src="images/africaine.jpg"></div>
-        <div id="contenu-article">
-            <h3>Titre article</h3>
-            <h4>> 30 octobre 2016</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel ligula quis velit mattis cursus a eu ex. Etiam tempus tristique ligula quis laoreet. Aliquam vitae rutrum mi. Nulla tincidunt id justo vitae accumsan. Mauris tempor ac lorem eget sodales. Quisque gravida, leo eu feugiat ullamcorper, leo sem tempus orci, non aliquam nibh lacus eu turpis. Donec ut feugiat felis. Cras vel neque eu erat malesuada venenatis vitae vitae nulla. Quisque mollis in risus eu varius.<br><br><span><a href="#">En savoir plus</a></span></p>
+<div id="contenu-page-accueil" class="col-lg-9">
+    <section id="partie-article" class="col-lg-8">
+        <h1>A la une</h1>
+        <article id="article" class="container">
+            <figure>
+                <img src="images/bouddha.jpg">
+            </figure>
+            <div id="contenu-article">
+                <h3>Titre article</h3>
+                <h4>> 30 octobre 2016</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel ligula quis velit mattis cursus a eu ex. Etiam tempus tristique ligula quis laoreet. Aliquam vitae rutrum mi. Nulla tincidunt id justo vitae accumsan. Mauris tempor ac lorem eget sodales. Quisque gravida, leo eu feugiat ullamcorper, leo sem tempus orci, non aliquam nibh lacus eu turpis. Donec ut feugiat felis. Cras vel neque eu erat malesuada venenatis vitae vitae nulla. Quisque mollis in risus eu varius.<br><br><span><a href="#">En savoir plus</a></span></p>
+            </div>
+        </article>
 
-        </div>
-    </article>
-    <article id="article">
-        <div id="image"><img src="images/africaine.jpg"></div>
-        <div id="contenu-article">
-            <h3>Titre article</h3>
-            <h4>> 30 octobre 2016</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel ligula quis velit mattis cursus a eu ex. Etiam tempus tristique ligula quis laoreet. Aliquam vitae rutrum mi. Nulla tincidunt id justo vitae accumsan. Mauris tempor ac lorem eget sodales. Quisque gravida, leo eu feugiat ullamcorper, leo sem tempus orci, non aliquam nibh lacus eu turpis. Donec ut feugiat felis. Cras vel neque eu erat malesuada venenatis vitae vitae nulla. Quisque mollis in risus eu varius.<br><br><span><a href="#">En savoir plus</a></span></p>
+        <article id="article" class="container">
+            <figure>
+                <img src="images/tigre.jpg">
+            </figure>
+            <div id="contenu-article">
+                <h3>Titre article</h3>
+                <h4>> 30 octobre 2016</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel ligula quis velit mattis cursus a eu ex. Etiam tempus tristique ligula quis laoreet. Aliquam vitae rutrum mi. Nulla tincidunt id justo vitae accumsan. Mauris tempor ac lorem eget sodales. Quisque gravida, leo eu feugiat ullamcorper, leo sem tempus orci, non aliquam nibh lacus eu turpis. Donec ut feugiat felis. Cras vel neque eu erat malesuada venenatis vitae vitae nulla. Quisque mollis in risus eu varius.<br><br><span><a href="#">En savoir plus</a></span></p>
+            </div>
+        </article>
 
-        </div>
-    </article>
-    <article id="article">
-        <div id="image"><img src="images/africaine.jpg"></div>
-        <div id="contenu-article">
-            <h3>Titre article</h3>
-            <h4>> 30 octobre 2016</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel ligula quis velit mattis cursus a eu ex. Etiam tempus tristique ligula quis laoreet. Aliquam vitae rutrum mi. Nulla tincidunt id justo vitae accumsan. Mauris tempor ac lorem eget sodales. Quisque gravida, leo eu feugiat ullamcorper, leo sem tempus orci, non aliquam nibh lacus eu turpis. Donec ut feugiat felis. Cras vel neque eu erat malesuada venenatis vitae vitae nulla. Quisque mollis in risus eu varius.<br><br><span><a href="#">En savoir plus</a></span></p>
+        <article id="article" class="container">
+            <figure>
+                <img src="images/africaine.jpg">
+            </figure>
+            <div id="contenu-article">
+                <h3>Titre article</h3>
+                <h4>> 30 octobre 2016</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel ligula quis velit mattis cursus a eu ex. Etiam tempus tristique ligula quis laoreet. Aliquam vitae rutrum mi. Nulla tincidunt id justo vitae accumsan. Mauris tempor ac lorem eget sodales. Quisque gravida, leo eu feugiat ullamcorper, leo sem tempus orci, non aliquam nibh lacus eu turpis. Donec ut feugiat felis. Cras vel neque eu erat malesuada venenatis vitae vitae nulla. Quisque mollis in risus eu varius.<br><br><span><a href="#">En savoir plus</a></span></p>
+            </div>
+        </article>
+    </section>
 
-        </div>
-    </article>
-</section>
-<aside  class="col-lg-offset-1 col-lg-2">
-    <h2>Derniers notés</h2>
-    <article id="dernier-note">
-        <img src="images/africaine.jpg">
-        <div id="dessous">
-            <ul>
-                <li class="titre-tableau">Africaine</li>
-                <li class="commentaire">3</li>
-                <li class="coeur">3</li>
-                <li class="aimer">3</li>
-            </ul>
-        </div>
-    </article>
-    <article id="dernier-note">
-        <img src="images/africaine.jpg">
-        <div id="dessous">
-            <ul>
-                <li class="titre-tableau">Africaine</li>
-                <li class="commentaire">3</li>
-                <li class="coeur">3</li>
-                <li class="aimer">3</li>
-            </ul>
-        </div>
-    </article>
-    <article id="dernier-note">
-        <img src="images/africaine.jpg">
-        <div id="dessous">
-            <ul>
-                <li class="titre-tableau">Africaine</li>
-                <li class="commentaire">3</li>
-                <li class="coeur">3</li>
-                <li class="aimer">3</li>
-            </ul>
-        </div>
-    </article>
-</aside>
 
+    <aside id="partie-dernier-note" class="container col-lg-3">
+        <h2>Derniers notés</h2>
+        <article id="dernier-note" class="">
+            <figure>
+                <img src="images/fleurs.jpg" alt="">
+            </figure>
+            <div class="info-img-dernier-note">
+                <ul>
+                    <li class="titre-tableau">Fleurs</li>
+                    <li class="icon-commentaire">3</li>
+                    <li class="icon-coeur">3</li>
+                    <li class="icon-aimer">3</li>
+                </ul>
+            </div>
+        </article>
+        <article id="dernier-note" class="">
+            <figure>
+                <img src="images/cheval.jpg" alt="">
+            </figure>
+            <div class="info-img-dernier-note">
+                <ul>
+                    <li class="titre-tableau">Cheval</li>
+                    <li class="icon-commentaire">3</li>
+                    <li class="icon-coeur">3</li>
+                    <li class="icon-aimer">3</li>
+                </ul>
+            </div>
+        </article>
+        <article id="dernier-note" class="">
+            <figure>
+                <img src="images/bateau.jpg" alt="">
+            </figure>
+            <div class="info-img-dernier-note">
+                <ul>
+                    <li class="titre-tableau">Bateau</li>
+                    <li class="icon-commentaire">3</li>
+                    <li class="icon-coeur">3</li>
+                    <li class="icon-aimer">3</li>
+                </ul>
+            </div>
+        </article>
+
+    </aside>
+</div>
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laure De Malibran</title>
-        <meta name="description" content="Artiste machin"/>
+    <meta name="description" content="Artiste machin"/>
     <meta name="keywords" content="artiste, machin"/>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -17,20 +17,24 @@
     <link href="css/style.css" rel=stylesheet media="screen"/>
 </head>
 <body>
-    <header>
-        <div class="container col-lg-2" id="menu">
-            <img class="logo" src="images/logoldm.png" alt="Logo Laure De Malibran"/>
+<header>
+    <div class="container col-lg-3" id="menu">
+        <img class="logo" src="images/logoldm.png" alt="Logo Laure De Malibran"/>
+        <nav id="onglet-menu">
             <ul>
-                <a href="index.php"><li class="choix">Accueil<div id="test"><div id="test2"></div></div></li></a>
+                <a href="index.php"><li class="onglet-choisi">Accueil</li></a>
                 <a href="biographie.php"><li>Biographie</li></a>
-                <li><a href="galerie.php">Galerie</a></li>
+                <a href="galerie.php"><li>Galerie</li></a>
                 <a href="contact.php"><li>Contact</li></a>
             </ul>
-            <div id="reseaux">
-                <a class="reseaux" href="#"><img src="images/facebook.png" alt="Reseaux sociaux : Facebook"/></a>
-                <a class="reseaux" href="#"><img src="images/pinterest.png" alt="Reseaux sociaux : Pinterest"/></a>
-            </div>
-            <p>Copyright &copy;<br>
-                <a href="#">Pierre-Yves Lotode</a> - <a href="#">Kevin Alary</a> - <a href="#">Claire Erhard</a></p>
+        </nav>
+        <figure id="reseaux-sociaux">
+            <a class="reseaux" href="#"><img src="images/facebook.png" alt="Reseaux sociaux : Facebook"/></a>
+            <a class="reseaux" href="#"><img src="images/pinterest.png" alt="Reseaux sociaux : Pinterest"/></a>
+        </figure>
+        <div id="copyright">
+        <p>Copyright &copy;<br>
+            <a href="#">Pierre-Yves Lotode</a> - <a href="#">Kevin Alary</a> - <a href="#">Claire Erhard</a></p>
         </div>
-    </header>
+    </div>
+</header>
